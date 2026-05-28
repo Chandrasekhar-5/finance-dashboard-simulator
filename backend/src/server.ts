c62eda1c2991e8d4b474a0bd3670b8ecc6afb34c
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-05-28 21:56:41 +0530

    server setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,16 @@
-import { env } from "node:process";
+import { env } from "./config/env.js";
 import app from "./app.js";
-import { PrismaClient } from '@prisma/client';
+import { prisma } from "./lib/prisma.js";
 
-
-const prisma = new PrismaClient();
 const PORT = env.PORT || 5000;
-
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running in ${env.NODE_ENV} mode on port ${PORT}`);
 });
 
-
 const shutdown = async (signal: string) => {
-    console.log(`\n ${signal} recieved. Shutting down gracefully...`);
-
-
+    console.log(`\n ${signal} received. Shutting down gracefully...`);
+    
     server.close(async () => {
         console.log('HTTP server closed');
         await prisma.$disconnect();
